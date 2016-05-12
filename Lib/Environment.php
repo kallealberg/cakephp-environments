24fd203c3aa2c0da3c9f40a1287c0632dc35b186
Kalle Alberg <kalle.alberg@gmail.com>
2016-05-12 12:40:44 +0200

    Update Environment.php

--- Lib/Environment.php
+++ Lib/Environment.php
@@ -1,4 +1,9 @@
 <?php
+namespace Josegonzalez\Environments;
+
+use Cake\Core\Configure;
+use Cake\Core\Exception\Exception;
+
 /**
  * Singleton class to handle environment specific configurations.
  *
@@ -11,129 +16,195 @@
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  */
-class Environment {
+class Environment
+{
 
-	public $environments = array();
+    public $environments = [];
 
-	protected static $_instance;
+    protected static $_instance;
 
-	protected $_configMap = array(
-		'security' => 'Security.level'
-	);
+    protected $_paramMap = [
+        'server' => 'SERVER_NAME'
+    ];
 
-	public static function &getInstance() {
-		if (! self::$_instance) {
-			$Environment = 'Environment';
-			if (config('app_environment')) {
-				$Environment = 'App' . $Environment;
-			}
+    /**
+     * Disables directly constructing an Environment instance
+     *
+     * @return void
+     **/
+    protected function __construct()
+    {
+    }
 
-			self::$_instance = new $Environment();
-			Configure::write('Environment.initialized', true);
-		}
+    /**
+     * Disables directly cloning an Environment instance
+     *
+     * @return void
+     **/
+    protected function __clone()
+    {
+    }
 
-		return self::$_instance;
-	}
+    /**
+     * Retrieves the current instance of an environment
+     *
+     * @return Josegonzalez\Environments\Environment
+     **/
+    public static function getInstance()
+    {
+        if (!self::$_instance) {
+            self::$_instance = new Environment();
+            Configure::write('Environment.initialized', true);
+        }
 
-	public static function configure($name, $params, $config = null, $callable = null) {
-		$_this = Environment::getInstance();
-		$_this->environments[$name] = compact('name', 'params', 'config', 'callable');
-	}
+        return self::$_instance;
+    }
 
-	public static function start($environment = null, $default = 'development') {
-		$_this =& Environment::getInstance();
-		return $_this->setup($environment, $default);
-	}
+    /**
+     * Configures a given environment with certain instructions
+     *
+     * @param string $name Name of the environment we are checking for
+     * @param array $params Array of keys to check
+     * @param array $config Array of configure keys
+     * @param callable $callable A callable to invoke when an environment matches
+     * @return bool
+     **/
+    public static function configure($name, $params, $config = null, $callable = null)
+    {
+        $_this = Environment::getInstance();
+        $_this->environments[$name] = compact('name', 'params', 'config', 'callable');
+    }
 
-	public static function is($environment = null) {
-		$current = Configure::read('Environment.name');
+    /**
+     * Kicks off the environment setup
+     *
+     * @param string $environment Name of the environment we want to force setup for
+     * @param string $default Default environment name
+     * @return bool
+     **/
+    public static function start($environment = null, $default = 'production')
+    {
+        $_this = Environment::getInstance();
+        return $_this->setup($environment, $default);
+    }
 
-		if (! $environment) {
-			return $current;
-		}
+    /**
+     * Checks if the current environment matches the passed environment
+     *
+     * @param string $environment Name of the environment we are checking for
+     * @return bool
+     **/
+    public static function is($environment = null)
+    {
+        $current = Configure::read('Environment.name');
 
-		return $current === $environment;
-	}
+        if (! $environment) {
+            return $current;
+        }
 
-	protected function __construct() {
-	}
+        return $current === $environment;
+    }
 
-	protected function __clone() {
-	}
+    /**
+     * Configures the current environment
+     *
+     * @param string $environment Name of the environment we want to force setup for
+     * @param string $default Default environment name
+     * @return bool
+     * @throws Cake\Core\Exception\Exception
+     **/
+    public function setup($environment = null, $default = 'production')
+    {
+        if (Configure::read('Environment.setup')) {
+            return false;
+        }
 
-/**
- * Configures the current environment
- *
- * @return boolean
- * @throws CakeException
- **/
-	public function setup($environment = null, $default = 'development') {
-		if (Configure::read('Environment.setup')) {
-			return false;
-		}
+        $current = $this->currentEnvironment($environment, $default);
 
-		$current = ($environment === null) ? $default : $environment;
-		if (empty($environment)) {
-			foreach ($this->environments as $name => $config) {
-				if ($this->_match($name, $config['params'])) {
-					$current = $name;
-					break;
-				}
-			}
-		}
+        if (!isset($this->environments[$current])) {
+            throw new Exception(sprintf('Environment %s does not exist.', $current));
+        }
 
-		if (!isset($this->environments[$current])) {
-			throw new CakeException(sprintf('Environment %s does not exist.', $current));
-		}
+        $config = array_merge(
+            $this->environments[$current]['config'],
+            ['Environment.name' => $current]
+        );
+        foreach ($config as $param => $value) {
+            Configure::write($param, $value);
+        }
 
-		$config = array_merge(
-			$this->environments[$current]['config'],
-			array('Environment.name' => $current)
-		);
-		foreach ($config as $param => $value) {
-			if (isset($this->_configMap[$param])) {
-				$param = $this->_configMap[$param];
-			}
-			Configure::write($param, $value);
-		}
+        if (is_callable($this->environments[$current]['callable'])) {
+            $this->environments[$current]['callable']();
+        }
 
-		if (is_callable($this->environments[$current]['callable'])) {
-			$this->environments[$current]['callable']();
-		}
+        Configure::write('Environment.setup', true);
 
-		Configure::write('Environment.setup', true);
+        return true;
+    }
 
-		return true;
-	}
+    /**
+     * Gets the current environment
+     *
+     * @param string $environment Name of the environment we want to force setup for
+     * @param string $default Default environment name
+     * @return string
+     **/
+    protected function currentEnvironment($environment = null, $default = 'production')
+    {
+        $current = ($environment === null) ? $default : $environment;
+        if (empty($environment)) {
+            foreach ($this->environments as $name => $config) {
+                if ($this->_match($name, $config['params'])) {
+                    $current = $name;
+                    break;
+                }
+            }
+        }
 
-	protected function _match($environment, $params) {
-		$cakeEnv = env('CAKE_ENV');
-		if (!empty($cakeEnv)) {
-			return env('CAKE_ENV') == $environment;
-		}
+        return $current;
+    }
 
-		if (is_bool($params)) {
-			return $params;
-		}
+    /**
+     * Matches the current setup to a given environment
+     *
+     * @param string $environment Name of the environment
+     * @param array $params Array of keys to check
+     * @return bool
+     **/
+    protected function _match($environment, $params)
+    {
+        $cakeEnv = env('CAKE_ENV');
+        if (!empty($cakeEnv)) {
+            return env('CAKE_ENV') == $environment;
+        }
 
-		if (is_callable($params) || (is_string($params) && function_exists($params))) {
-			return $params();
-		}
+        if (is_bool($params)) {
+            return $params;
+        }
 
-		foreach ($params as $param => $value) {
-			if (function_exists($param)) {
-				$match = call_user_func($param, $value);
-			} elseif (is_array($value)) {
-				$match = in_array(env($param), $value);
-			} else {
-				$match = (env($param) === $value);
-			}
+        if (is_callable($params) || (is_string($params) && function_exists($params))) {
+            return $params();
+        }
 
-			if (!$match) {
-				return false;
-			}
-		}
-		return true;
-	}
+        foreach ($params as $param => $value) {
 
+            if (isset($this->_paramMap[$param])) {
+                $param = $this->_paramMap[$param];
+            }
+
+
+            if (function_exists($param)) {
+                $match = call_user_func($param, $value);
+            } elseif (is_array($value)) {
+                $match = in_array(env($param), $value);
+            } else {
+                $match = (env($param) === $value);
+            }
+
+            if (!$match) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
